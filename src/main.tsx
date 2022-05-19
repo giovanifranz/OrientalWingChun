bd1bb67e08e17b2e99d3cb0f7243bebcf19718c1
giovani.franz <giovani.franz@agi.com.br>
2022-05-19 17:07:15 -0300

    refactor: linters and main.ts

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,7 @@ import { GlobalStyle } from '@/styles'
 
 import '@/styles/zector.css'
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <GlobalStyle />
     <App />
